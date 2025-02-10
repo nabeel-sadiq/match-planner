371bdbf28cfb094fa4b38b554a3875d6f9b970f9
nabeel-sadiq <nabeelsadiq.dev@gmail.com>
2025-02-10 21:12:33 +0500

    tried fix schema

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,11 +1,15 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./server/db/index.js"; // your drizzle instance
+import { schema } from "./schema";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "sqlite", // or "mysql", "pg"
-    schema: "./server/db/schema.js"
+    schema: {
+      ...schema,
+      user: schema.user,
+    },
   }),
   emailAndPassword: {
     enabled: true,
